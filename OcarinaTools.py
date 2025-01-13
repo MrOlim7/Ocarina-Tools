3c721e2885f745a68c444b14c1fc420573d1dc10
OlimDev <mrolimyt@gmail.com>
2025-01-13 19:11:49 +0100

    Add files via upload

--- OcarinaTools.py
+++ OcarinaTools.py
@@ -1,10 +1,18 @@
 from pystyle import Colors, Colorate, Center
 from colorama import Fore, Back, Style
 import colorama
-import sys
+import subprocess
 
-print("Python Path:", sys.executable)
-print("Sys Path:", sys.path)
+
+def ping_ip(ip_address):
+    try:
+        result = subprocess.run(['ping', ip_address], capture_output=True, text=True, timeout=10)
+        print(Colorate.Horizontal(Colors.blue_to_red, f"\n{'=' * 60}\nPINGING {ip_address}\n{'=' * 60}"))
+        print(result.stdout)
+    except subprocess.TimeoutExpired:
+        print(Colorate.Horizontal(Colors.blue_to_red, "Timeout expired. No response received."))
+    except Exception as e:
+        print(Colorate.Horizontal(Colors.blue_to_red, f"An error occurred: {e}"))
 
 
 ascii_art = '''
@@ -31,11 +39,11 @@ ascii_art = '''
 colored_ascii = Colorate.Horizontal(Colors.blue_to_purple, ascii_art)
 print(Center.XCenter(colored_ascii))
 
-                                                                                                                                                          
-                                                                                                                                                          
-                                                                                                                                                        
-                                                                                                                                                        
-
+while True:
+    option = input(Colorate.Horizontal(Colors.blue_to_purple, "\nEnter your choice: "))
+    if option == "01":
+         ip_address = input(Colorate.Horizontal(Colors.blue_to_red, "Enter IP address to ping: "))
+         ping_ip(ip_address)  
 
 if __name__ == "__main__":
-    main()
+    main()                                                                                                                                                                                                                                                                                                                                                                                                                                           
